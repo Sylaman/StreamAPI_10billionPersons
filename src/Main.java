1216031b5644e876af2226d28a1455f36c516694
Sylaman <Sylaman.snake@gmail.com>
2022-09-03 15:02:43 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,5 +16,24 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
+        // 1. Найти количество несовершеннолетних (т.е. людей младше 18 лет).
+        long minorsAmount = persons.stream()
+                .filter(p -> p.getAge() < 18)
+                .count();
+
+        // 2. Получить список фамилий призывников (т.е. мужчин от 18 и до 27 лет).
+        List<String> recruitsFamilies = persons.stream()
+                .filter(p -> p.getAge() >= 18 && p.getAge() <= 27 && p.getSex() == Sex.MAN)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+
+        // 3. Получить отсортированный по фамилии список потенциально работоспособных людей с высшим образованием
+        List<Person> peopleFitForWork = persons.stream()
+                .filter(p -> p.getEducation() == Education.HIGHER)
+                .filter(p -> p.getAge() >= 18)
+                .filter(p -> (p.getSex() == Sex.MAN && p.getAge() <= 65) ||
+                        (p.getSex() == Sex.WOMAN && p.getAge() <= 60))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
     }
 }
